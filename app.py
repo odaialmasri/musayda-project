f4f1c54f4c9858c2f71645e05b988ca2d679d5a1
Mohammed Hijazi <mohammedhijazi199@gmail.com>
2017-07-25 09:20:53 +0300

    pull index

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import dataset
 
 app = Flask(__name__)
 
-db=dataset.connect("postgres://yowahpfaxtsjoc:89e78de430594fa7bffec234ce5bc8ac9193cef0a864378fd900ddd573d65651@ec2-23-23-244-83.compute-1.amazonaws.com:5432/dbn2rcviq5s3i4")
+# db=dataset.connect("postgres://yowahpfaxtsjoc:89e78de430594fa7bffec234ce5bc8ac9193cef0a864378fd900ddd573d65651@ec2-23-23-244-83.compute-1.amazonaws.com:5432/dbn2rcviq5s3i4")
 
-table=db["musa3ada_users"]
+# table=db["musa3ada_users"]
 
 
 @app.route("/")
@@ -38,11 +38,11 @@ def form_posts():
 	user_phonenumber = request.form["phone_number"]
 	user_status = request.form["msg"]
 
-	user1 = musa3ada_users.insert(dict(username=user_name,Email=user_email,phonenumber=user_phonenumber,user_info=user_status))
+	# user1 = musa3ada_users.insert(dict(username=user_name,Email=user_email,phonenumber=user_phonenumber,user_info=user_status))
 
-	user_list=list(user1)
+	# user_list=list(user1)
 
-	return render_template("view.html",name1=user_list)
+	return render_template("view.html",name1=user_name,email1=user_email,phonenumber1=user_phonenumber,info1=user_status)
 
 
 
